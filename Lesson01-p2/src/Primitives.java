706ea6d807cbca9f7218a8f5dd263de6f86506bf
Al Farhaan khan I Inamdar <63239385+Zouziszzm@users.noreply.github.com>
2023-12-18 12:54:06 +0530

    primitives push.
    inital primitives.

---
+++ Lesson01-p2/src/Primitives.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Primitives {
+  public static void main(String[] args) {
+    int rollnumber = 64;
+    char letter = 'r';
+    float marks = 98.0f;
+    double largedecimalnumbers = 4567469.4579;
+    long largeIntegervalues = 34252345245L;
+
+    //not primitive
+    String Name = "Farhaan"
+  }
+}
