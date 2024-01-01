5c0bb9f6ea46bb75e3dda14b6e271c5a657197ab
Al Farhaan khan I Inamdar <63239385+Zouziszzm@users.noreply.github.com>
2024-01-01 21:25:19 +0530

    dsa.
    Java Program first

--- Lesson01-p2/src/Primitives.java
+++ Lesson01-p2/src/Primitives.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 
 public class Primitives {
   public static void main(String[] args) {
-    int rollnumber = 64;
-    char letter = 'r';
-    float marks = 98.0f;
-    double largedecimalnumbers = 4567469.4579;
-    long largeIntegervalues = 34252345245L;
-    boolean check = false;
-     
-    //not primitive
-    String Name = "Farhaan"
+    // int rollnumber = 64;
+    // char letter = 'r';
+    // float marks = 98.0f; // why we add the f we use it for larger values and
+    // dosen't give error in some
+    // instances.
+    // double largedecimalnumbers = 4567469.4579;
+    // long largeIntegervalues = 34252345245L;
+    // boolean check = false;
+
+    // not primitive
+    // String Name = "Farhaan";
+    //
+
   }
 }
---
+++ Lesson_2/Inputs.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Inputs {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("please entera some inputs ");
+    int rollno = input.nextInt();a
+    System.out.println("Your roll no is " + rollno);
+  }
+
+}
